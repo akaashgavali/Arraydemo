e35c12c6b5ac6b963c568408939e2f36947d7a33
akaashgavali <akashgavali4546@gmail.com>
2022-10-18 22:16:30 +0530

    commit

--- Arraydemo/OccuranceArray.cs
+++ Arraydemo/OccuranceArray.cs
@@ -1,31 +1,34 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-////Count number of occurrences of a number in an array [1,2,3,1,3,4,5]
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Count number of occurrences of a number in an array [1,2,3,1,3,4,5]
 
-//namespace Arraydemo
-//{
-//    internal class OccuranceArray
-//    {
+namespace Arraydemo
+{
+    internal class OccuranceArray
+    {
 
-//        static void Main(string[] args)
-//        {
-//            int[] number = { 1, 2,3,1,3,4,5 };
-//            int[] num = 0;
-           
-//            int count = 0;
-//            for (int i = 0; i < number.Length; i++)
-//            {
-//                if (number[i] == number.Length)
-//                {
-//                    count++;
-//                }
-//                Console.WriteLine($"number count{count}");
-//            }
+        static void Main(string[] args)
+        {
+            int[] number = { 1, 2, 3, 1, 3, 4, 5 };
+            
+            for(int i=0;i<number.Length;i++)
+            {
+                for(int j=i+1;j<number.Length;j++)
+                {
+                    if (number[i] == number[j])
+                    {
+                        Console.WriteLine(number[i]);
+                    }
+                }
+
+            }
+            
             
 
-//        }
-//    }
-//}
+
+        }
+    }
+}
--- Arraydemo/ReverseArray.cs
+++ Arraydemo/ReverseArray.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Arraydemo
-{
-    internal class ReverseArray
-    {
-        static void Main(string[] args)
-        {
-            int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//namespace Arraydemo
+//{
+//    internal class ReverseArray
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            for (int i = number.Length; i>=0; i--)
-            {
-                    Console.WriteLine(i);
+//            for (int i = number.Length; i>=0; i--)
+//            {
+//                    Console.WriteLine(i);
                 
-            }
-        }
-    }
-}
+//            }
+//        }
+//    }
+//}
